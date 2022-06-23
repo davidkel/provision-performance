e2576847b322e1d4eab55cc111521a3e8c2bead3
D <d_kelsey@uk.ibm.com>
2022-06-23 17:03:45 +0100

    readme and minor code improvement
    Signed-off-by: D <d_kelsey@uk.ibm.com>

--- Manager/run-benchmark.sh
+++ Manager/run-benchmark.sh
@@ -19,7 +19,7 @@ function read_input {
     while [[ -z $INPUT ]]
     do
         read -e -p "$1" INPUT
-        if [ $# -eq 2 ]; then 
+        if [ $# -eq 2 ]; then
             INPUT=${INPUT:-"$2"}
         fi
     done
@@ -48,13 +48,12 @@ else
     REMOTE="local"
 fi
 WORKERS=$(read_input "Number of worker [10]: " "10")
-CONTROLLER=$(read_input "Rate Controller to use [fixed-load]: " "fixed-load")
 file_exists "Benchmark file relative path [create-asset]: " "../../caliper-benchmarks/benchmarks/api/fabric/create-asset.yaml"
 BENCHMARK_FILE=$FILENAME
 
 DIR="$(dirname "$(realpath "$0")")"
 BENCHMARK=$DIR/benchmark.yaml
-echo "$(expand_template $REMOTE $WORKERS $CONTROLLER)" > $BENCHMARK
+echo "$(expand_template $REMOTE $WORKERS)" > $BENCHMARK
 
 if [ $REMOTE == "local" ]; then
     pushd ../Worker
@@ -62,11 +61,11 @@ if [ $REMOTE == "local" ]; then
     popd
     ./launch-manager.sh $BENCHMARK -l
 else
+    echo "If you haven't done so already, start your remote caliper workers now"
     ./launch-manager.sh $BENCHMARK
 fi
+
 #TODO:
-# specify a rate controller and it's parameters update the benchmark file dynamically
-# launch remote workers //can we start remote workers from the manager vm?
-# launch local workers //this will happen in the ./launch-workers.sh if we pass the -l flag
-# do we want to not separate remote from local and always use mosquitto and just ask for how many local and remote the user wants and connect them all to mosqitto
-# add substitution of rate controller and its options as well
+# specify a rate controller and it's parameters update the benchmark file dynamically, or at least support changing it tps and load for both fixed-tps, fixed-load
+# maybe we use template benchmarks that just fill in values as specified
+# launch remote workers
