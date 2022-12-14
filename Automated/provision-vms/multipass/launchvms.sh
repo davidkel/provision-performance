2f7435ab6474b9fc0e2ba7b0f916b7d95b9a46f2
Dave Kelsey <d_kelsey@uk.ibm.com>
2022-12-14 16:31:48 +0000

    fix bug where could not interact with docker for playbooks
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- Automated/provision-vms/multipass/launchvms.sh
+++ Automated/provision-vms/multipass/launchvms.sh
@@ -4,3 +4,12 @@
 multipass launch -c 1 -d 10G -m 2G -n peer0
 multipass launch -c 2 -d 5G -m 1G -n client0 #
 multipass launch -c 2 -d 5G -m 1G -n client1 # for worker
+
+
+# inside vm
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# chmod +x get-docker.sh
+# ./get-docker.sh
+# sudo usermod -aG docker $USER
+
+#install docker compose ?
